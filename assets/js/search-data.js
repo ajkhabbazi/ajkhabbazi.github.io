7ecc7afd5326b164fe33646bf76d46a5309e9b72
ajkhabbazi <76255347+ajkhabbazi@users.noreply.github.com>
2026-03-07 07:16:09 +0000

    Deploying to gh-pages from @ ajkhabbazi/ajkhabbazi.github.io@778626bf2988e97fdf14e83bd6489951d108d039 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -78,7 +78,7 @@ ninja.data = [{
           title: 'I received the Graduate Student Grant-in-Aid Award from the American Society of Heating,...',
           description: "",
           section: "News",},{id: "news-i-joined-tesla-as-an-applied-ml-intern-on-the-drive-unit-team",
-          title: 'I joined Tesla as an Applied ML Intern on the Drive Unit team!...',
+          title: 'I joined Tesla as an Applied ML Intern on the Drive Unit team!⚡...',
           description: "",
           section: "News",},{id: "news-our-review-paper-lessons-learned-from-field-demonstrations-of-model-predictive-control-and-reinforcement-learning-for-residential-and-commercial-hvac-a-review-was-published-in-applied-energy-my-first-phd-journal-paper",
           title: 'Our review paper “Lessons learned from field demonstrations of model predictive control and...',
